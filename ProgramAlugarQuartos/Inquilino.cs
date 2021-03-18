7c5be20d0d92920f1dc26a3cf2779e5e92fa1ff0
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-18 18:08:18 -0300

    Add project files.

---
+++ ProgramAlugarQuartos/Inquilino.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ProgramAlugarQuartos
+{
+    class Inquilino
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+
+        public override string ToString()
+        {
+            return "\n" + Quarto + ": " + Nome + ", " + Email;
+        }
+
+
+    }
+
+
+
+}
---
+++ ProgramAlugarQuartos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramAlugarQuartos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inquilino[] inquilinoVect = new Inquilino[10];
+
+            Console.Write("Quantos quartos vão ser alugado? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\nNome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                inquilinoVect[quarto] = new Inquilino { Nome = nome, Email = email, Quarto = quarto };
+
+            }
+
+            Console.Write("\nQuartos ocupados: ");
+            for (int i = 0; i < inquilinoVect.Length; i++)
+            {
+                if (inquilinoVect[i] != null)
+                {
+                    Console.Write(inquilinoVect[i]);
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
